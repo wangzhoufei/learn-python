53f2ffcc3647a8bc0830f45c2264450dc82d5f66
fubuki.nishikado <fubuki.nishikado@gmail.com>
2020-04-29 18:48:58 +0900

    test

--- Sources/testPython.py
+++ Sources/testPython.py
@@ -1,3 +1,7 @@
-from builtins import print
+import math
+import itertools
 
-print("hello world.")
+for a, b in itertools.product(range(5, 90, 5), range(1, 100)):
+    if math.gcd(a, b) == 5 and a * b // math.gcd(a, b) == 90:
+        print("a={},b={}".format(a, b))
+        continue
